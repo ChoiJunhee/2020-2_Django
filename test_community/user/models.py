5d1d59c6dcddaa06e04affd832b7491ce912fe58
ChoiJunhee <python@hanyang.ac.kr>
2020-10-23 08:50:42 +0900

    2020.10.23 08:50

--- test_community/user/models.py
+++ test_community/user/models.py
@@ -7,5 +7,10 @@ class user(models.Model):
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_time = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
 
+    def __str__(self):
+        return self.username
+
     class Meta:
         db_table = 'community_user'
+        verbose_name ='커뮤니티 사용자'
+        verbose_name_plural = '커뮤니티 사용자'
